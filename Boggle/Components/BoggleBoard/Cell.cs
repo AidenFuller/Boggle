d6d7384ec6252cbec54a3e72e6825a4d3a0d46a2
TheCosmicNomad <aidenfuller314@gmail.com>
2024-03-02 17:59:57 +1100

    Boggle Solver

---
+++ Boggle/Components/BoggleBoard/Cell.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace Boggle.Components.BoggleBoard;
+
+public class Cell
+{
+    public string Letter { get; set; }
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public bool IsHighlighted { get; set; } = false;
+    
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Column);
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is Cell cell)
+        {
+            return cell.Row == Row && cell.Column == Column;
+        }
+        return false;
+    }
+}
